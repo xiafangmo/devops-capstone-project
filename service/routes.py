8fa4f5fe8006c29f2cab3fd9b9af73a746b55c34
Xiafang Mo <xiafang.mo@gmail.com>
2023-04-24 22:38:50 +0530

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -110,7 +110,6 @@ def update_accounts(account_id):
 ######################################################################
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
- 
     app.logger.info("Request to delete an Account with id: %s", account_id)
 
     account = Account.find(account_id)
